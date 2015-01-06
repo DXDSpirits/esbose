121b21daac24bccb47f1b2e487175814bffaa0b2
Xindong Ding <dxd.spirits@gmail.com>
2015-01-06 23:05:21 +0800

    style jobs category page

---
+++ assets/javascripts/JOBS.js
@@ -0,0 +1,42 @@
+(function() {
+	window.JOBS = [
+		{
+			id: 1,
+			category: 'RDG(零售业务部)',
+			positions: [
+				{id: 100, name: "District Manager<br>(区域零售业务经理)", location:"Shanghai"},
+				{id: 101, name: "Store Manager<br>(专卖店店长)", location:"Shenyang"},
+				{id: 102, name: "Store Manager<br>(专卖店店长)", location:"Wuxi"},
+				{id: 103, name: "Store Manager<br>(专卖店店长)", location:"Shanghai"},
+				{id: 104, name: "Store Sales<br>(专卖店店员)", location:"Hangzhou"},
+				{id: 105, name: "Store Sales<br>(专卖店店员)", location:"Suzhou"},
+				{id: 106, name: "Store Sales<br>(专卖店店员)", location:"Wuxi"},
+				{id: 107, name: "Store Sales<br>(专卖店店员)", location:"Shenyang"},
+				{id: 108, name: "Store Sales<br>(专卖店店员)", location:"Tianjin"},
+				{id: 109, name: "Store Sales<br>(专卖店店员)", location:"Changchun"}
+			]
+		}, {
+			id: 2,
+			category: 'Function',
+			positions: [
+				{
+					id: 200, name: 'Sales Training Manager', location: 'Shanghai'
+				}, {
+					id: 200, name: 'Logistic Assistant', location: 'Shanghai'
+				}
+			]
+		}, {
+			id: 3,
+			category: 'PSD(专业系统部)',
+			positions: [
+				{
+					id: 300, name: 'Dealer Account Manager<br>(Jiangsu, Anhui region)', location: 'Shanghai'
+				}, {
+					id: 301, name: 'Dealer Account Manager<br>(Zhejiang region)', location: 'Shanghai'
+				}, {
+					id: 302, name: 'Operation Assistant', location: 'Shanghai'
+				}
+			]
+		}
+	];
+})();
