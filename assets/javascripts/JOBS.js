b14cbf85be3ae7d44c0b9809ff9a3bde5ea1522a
Xindong Ding <dxd.spirits@gmail.com>
2015-01-06 23:15:42 +0800

    style page-job-detail

--- assets/javascripts/JOBS.js
+++ assets/javascripts/JOBS.js
@@ -4,39 +4,38 @@
 			id: 1,
 			category: 'RDG(零售业务部)',
 			positions: [
-				{id: 100, name: "District Manager<br>(区域零售业务经理)", location:"Shanghai"},
-				{id: 101, name: "Store Manager<br>(专卖店店长)", location:"Shenyang"},
-				{id: 102, name: "Store Manager<br>(专卖店店长)", location:"Wuxi"},
-				{id: 103, name: "Store Manager<br>(专卖店店长)", location:"Shanghai"},
-				{id: 104, name: "Store Sales<br>(专卖店店员)", location:"Hangzhou"},
-				{id: 105, name: "Store Sales<br>(专卖店店员)", location:"Suzhou"},
-				{id: 106, name: "Store Sales<br>(专卖店店员)", location:"Wuxi"},
-				{id: 107, name: "Store Sales<br>(专卖店店员)", location:"Shenyang"},
-				{id: 108, name: "Store Sales<br>(专卖店店员)", location:"Tianjin"},
-				{id: 109, name: "Store Sales<br>(专卖店店员)", location:"Changchun"}
+				{id: 100, nameEN: 'District Manager', name: '区域零售业务经理', location:'Shanghai'},
+				{id: 101, nameEN: 'Store Manager', name: '专卖店店长', location:'Shenyang'},
+				{id: 102, nameEN: 'Store Manager', name: '专卖店店长', location:'Wuxi'},
+				{id: 103, nameEN: 'Store Manager', name: '专卖店店长', location:'Shanghai'},
+				{id: 104, nameEN: 'Store Sales', name: '专卖店店员', location:'Hangzhou'},
+				{id: 105, nameEN: 'Store Sales', name: '专卖店店员', location:'Suzhou'},
+				{id: 106, nameEN: 'Store Sales', name: '专卖店店员', location:'Wuxi'},
+				{id: 107, nameEN: 'Store Sales', name: '专卖店店员', location:'Shenyang'},
+				{id: 108, nameEN: 'Store Sales', name: '专卖店店员', location:'Tianjin'},
+				{id: 109, nameEN: 'Store Sales', name: '专卖店店员', location:'Changchun'}
 			]
 		}, {
 			id: 2,
 			category: 'Function',
 			positions: [
-				{
-					id: 200, name: 'Sales Training Manager', location: 'Shanghai'
-				}, {
-					id: 200, name: 'Logistic Assistant', location: 'Shanghai'
-				}
+				{id: 200, nameEN: 'Sales Training Manager', name: '培训经理', location: 'Shanghai'},
+				{id: 201, nameEN: 'Logistic Assistant', name: '物流助理', location: 'Shanghai'}
 			]
 		}, {
 			id: 3,
 			category: 'PSD(专业系统部)',
 			positions: [
-				{
-					id: 300, name: 'Dealer Account Manager<br>(Jiangsu, Anhui region)', location: 'Shanghai'
-				}, {
-					id: 301, name: 'Dealer Account Manager<br>(Zhejiang region)', location: 'Shanghai'
-				}, {
-					id: 302, name: 'Operation Assistant', location: 'Shanghai'
-				}
+				{id: 300, nameEN: 'Dealer Account Manager', name: '渠道客户经理（江苏、安徽区域）', location: 'Shanghai'},
+				{id: 301, nameEN: 'Dealer Account Manager', name: '渠道客户经理（浙江区域）', location: 'Shanghai'},
+				{id: 302, nameEN: 'Operation Assistant', name: '业务助理', location: 'Shanghai'}
 			]
 		}
 	];
+	window.JOB_POSITIONS = {};
+	_.each(JOBS, function(category) {
+		_.each(category.positions, function(position) {
+			JOB_POSITIONS[position.id] = position
+		});
+	})
 })();
