1ba6cf083559c597069fe704a88178140c6cb77a
Xindong Ding <dxd.spirits@gmail.com>
2015-01-07 12:30:05 +0800

    style cars brand menu

--- assets/javascripts/CARS.js
+++ assets/javascripts/CARS.js
@@ -1,226 +1,328 @@
-
 (function() {
     window.CARS = [{
-        name : "奥迪",
-        url : "http://bose.cn/auto_list.aspx?flid=282",
-        series : [{
-            name : "Q3",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2356"
+        "name" : "奥迪",
+        "url" : "http://bose.cn/auto_list.aspx?flid=282",
+        "id" : 282,
+        "series" : [{
+            "name" : "Q3",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2356",
+            "id" : 2356
         }, {
-            name : "A6L/S6",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2353"
+            "name" : "A6L/S6",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2353",
+            "id" : 2353
         }, {
-            name : "Audi A7",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2354"
+            "name" : "Audi A7",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2354",
+            "id" : 2354
         }, {
-            name : "A8L/S8",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2355"
+            "name" : "A8L/S8",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2355",
+            "id" : 2355
         }, {
-            name : "Audi A1",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2352"
+            "name" : "Audi A1",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2352",
+            "id" : 2352
         }, {
-            name : "Q7",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2357"
+            "name" : "Q7",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2357",
+            "id" : 2357
         }, {
-            name : "TT Coupe (S)",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2358"
+            "name" : "TT Coupe (S)",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2358",
+            "id" : 2358
         }, {
-            name : "TT Roadster (S)",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2359"
-        }]
+            "name" : "TT Roadster (S)",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2359",
+            "id" : 2359
+        }],
+        "image" : "http://bose.cn/upfile/20145716025740_1.jpg"
     }, {
-        name : "保时捷",
-        url : "http://bose.cn/auto_list.aspx?flid=291",
-        series : [{
-            name : "Cayman",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2531"
+        "name" : "别克",
+        "url" : "http://bose.cn/auto_list.aspx?flid=283",
+        "id" : 283,
+        "series" : [{
+            "name" : "Enclave昂科雷",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2501",
+            "id" : 2501
         }, {
-            name : "Boxster",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2532"
+            "name" : "GL8 豪华商务车",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2502",
+            "id" : 2502
         }, {
-            name : "911 Carrera",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2533"
+            "name" : "LaCrosse君越",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2503",
+            "id" : 2503
         }, {
-            name : "Panamera",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2534"
+            "name" : "Regal君威",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2504",
+            "id" : 2504
         }, {
-            name : "Cayenne卡宴",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2535"
-        }, {
-            name : "Macan",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2536"
-        }]
+            "name" : "Envision昂科威",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=3028",
+            "id" : 3028
+        }],
+        "image" : "http://bose.cn/upfile/20140616110604_1.jpg"
     }, {
-        name : "别克",
-        url : "http://bose.cn/auto_list.aspx?flid=283",
-        series : [{
-            name : "Enclave昂科雷",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2501"
+        "name" : "红旗",
+        "url" : "http://bose.cn/auto_list.aspx?flid=284",
+        "id" : 284,
+        "series" : [{
+            "name" : "红旗H7",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2505",
+            "id" : 2505
         }, {
-            name : "GL8 豪华商务车",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2502"
-        }, {
-            name : "LaCrosse君越",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2503"
-        }, {
-            name : "Regal君威",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2504"
-        }, {
-            name : "Envision昂科威",
-            url : "http://bose.cn/auto_listpage.aspx?nid=3028"
-        }]
+            "name" : "红旗L5",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=3035",
+            "id" : 3035
+        }],
+        "image" : "http://bose.cn/upfile/20140716110706_1.jpg"
     }, {
-        name : "红旗",
-        url : "http://bose.cn/auto_list.aspx?flid=284",
-        series : [{
-            name : "红旗H7",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2505"
-        }, {
-            name : "红旗L5",
-            url : "http://bose.cn/auto_listpage.aspx?nid=3035"
-        }]
+        "name" : "雪佛兰",
+        "url" : "http://bose.cn/auto_list.aspx?flid=285",
+        "id" : 285,
+        "series" : [{
+            "name" : "Volt",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2506",
+            "id" : 2506
+        }],
+        "image" : "http://bose.cn/upfile/20140716110758_1.jpg"
     }, {
-        name : "凯迪拉克",
-        url : "http://bose.cn/auto_list.aspx?flid=292",
-        series : [{
-            name : "ATS-L",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2537"
+        "name" : "马自达",
+        "url" : "http://bose.cn/auto_list.aspx?flid=286",
+        "id" : 286,
+        "series" : [{
+            "name" : "Mazda MX-5",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2507",
+            "id" : 2507
         }, {
-            name : "CTS",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2538"
+            "name" : "Mazda ATENZA阿特兹",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2509",
+            "id" : 2509
         }, {
-            name : "XTS",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2539"
+            "name" : "Mazda CX-5",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2510",
+            "id" : 2510
         }, {
-            name : "SRX",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2540"
-        }, {
-            name : "Escalade Hybrid",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2541"
-        }]
+            "name" : "Mazda CX-7",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=3103",
+            "id" : 3103
+        }],
+        "image" : "http://bose.cn/upfile/20140816110808_1.jpg"
     }, {
-        name : "雷诺",
-        url : "http://bose.cn/auto_list.aspx?flid=289",
-        series : [{
-            name : "Latitude纬度",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2527"
+        "name" : "日产",
+        "url" : "http://bose.cn/auto_list.aspx?flid=287",
+        "id" : 287,
+        "series" : [{
+            "name" : "Teana 天籁",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2511",
+            "id" : 2511
         }, {
-            name : "Talisman塔利斯曼",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2528"
+            "name" : "Teana 天籁▪公爵",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2512",
+            "id" : 2512
         }, {
-            name : "Koleos科雷傲",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2529"
-        }]
+            "name" : "GTR",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2513",
+            "id" : 2513
+        }, {
+            "name" : "370Z",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2514",
+            "id" : 2514
+        }, {
+            "name" : "QUEST 贵士",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2515",
+            "id" : 2515
+        }, {
+            "name" : "MURANO 楼兰",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2516",
+            "id" : 2516
+        }, {
+            "name" : "Patrol途乐",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2517",
+            "id" : 2517
+        }],
+        "image" : "http://bose.cn/upfile/20140816110828_1.jpg"
     }, {
-        name : "马自达",
-        url : "http://bose.cn/auto_list.aspx?flid=286",
-        series : [{
-            name : "Mazda MX-5",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2507"
+        "name" : "英菲尼迪",
+        "url" : "http://bose.cn/auto_list.aspx?flid=288",
+        "id" : 288,
+        "series" : [{
+            "name" : "Q50",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2518",
+            "id" : 2518
         }, {
-            name : "Mazda ATENZA阿特兹",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2509"
+            "name" : "Q60S",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2519",
+            "id" : 2519
         }, {
-            name : "Mazda CX-5",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2510"
+            "name" : "Q60",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2520",
+            "id" : 2520
         }, {
-            name : "Mazda CX-7",
-            url : "http://bose.cn/auto_listpage.aspx?nid=3103"
-        }]
+            "name" : "Q70L",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2521",
+            "id" : 2521
+        }, {
+            "name" : "QX50",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2522",
+            "id" : 2522
+        }, {
+            "name" : "QX60",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2523",
+            "id" : 2523
+        }, {
+            "name" : "QX70",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2524",
+            "id" : 2524
+        }, {
+            "name" : "QX80",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2525",
+            "id" : 2525
+        }],
+        "image" : "http://bose.cn/upfile/20140916110910_1.jpg"
     }, {
-        name : "日产",
-        url : "http://bose.cn/auto_list.aspx?flid=287",
-        series : [{
-            name : "Teana 天籁",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2511"
+        "name" : "雷诺",
+        "url" : "http://bose.cn/auto_list.aspx?flid=289",
+        "id" : 289,
+        "series" : [{
+            "name" : "Latitude纬度",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2527",
+            "id" : 2527
         }, {
-            name : "Teana 天籁▪公爵",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2512"
+            "name" : "Talisman塔利斯曼",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2528",
+            "id" : 2528
         }, {
-            name : "GTR",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2513"
-        }, {
-            name : "370Z",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2514"
-        }, {
-            name : "QUEST 贵士",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2515"
-        }, {
-            name : "MURANO 楼兰",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2516"
-        }, {
-            name : "Patrol途乐",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2517"
-        }]
+            "name" : "Koleos科雷傲",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2529",
+            "id" : 2529
+        }],
+        "image" : "http://bose.cn/upfile/20140916110917_1.jpg"
     }, {
-        name : "绅宝",
-        url : "http://bose.cn/auto_list.aspx?flid=290",
-        series : [{
-            name : "D70",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2530"
+        "name" : "绅宝",
+        "url" : "http://bose.cn/auto_list.aspx?flid=290",
+        "id" : 290,
+        "series" : [{
+            "name" : "D70",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2530",
+            "id" : 2530
         }, {
-            name : "D60",
-            url : "http://bose.cn/auto_listpage.aspx?nid=3049"
-        }]
+            "name" : "D60",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=3049",
+            "id" : 3049
+        }],
+        "image" : "http://bose.cn/upfile/20140916110925_1.jpg"
     }, {
-        name : "雪佛兰",
-        url : "http://bose.cn/auto_list.aspx?flid=285",
-        series : [{
-            name : "Volt",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2506"
-        }]
+        "name" : "保时捷",
+        "url" : "http://bose.cn/auto_list.aspx?flid=291",
+        "id" : 291,
+        "series" : [{
+            "name" : "Cayman",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2531",
+            "id" : 2531
+        }, {
+            "name" : "Boxster",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2532",
+            "id" : 2532
+        }, {
+            "name" : "911 Carrera",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2533",
+            "id" : 2533
+        }, {
+            "name" : "Panamera",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2534",
+            "id" : 2534
+        }, {
+            "name" : "Cayenne卡宴",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2535",
+            "id" : 2535
+        }, {
+            "name" : "Macan",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2536",
+            "id" : 2536
+        }],
+        "image" : "http://bose.cn/upfile/20140916110932_1.jpg"
     }, {
-        name : "英菲尼迪",
-        url : "http://bose.cn/auto_list.aspx?flid=288",
-        series : [{
-            name : "Q50",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2518"
+        "name" : "凯迪拉克",
+        "url" : "http://bose.cn/auto_list.aspx?flid=292",
+        "id" : 292,
+        "series" : [{
+            "name" : "ATS-L",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2537",
+            "id" : 2537
         }, {
-            name : "Q60S",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2519"
+            "name" : "CTS",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2538",
+            "id" : 2538
         }, {
-            name : "Q60",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2520"
+            "name" : "XTS",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2539",
+            "id" : 2539
         }, {
-            name : "Q70L",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2521"
+            "name" : "SRX",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2540",
+            "id" : 2540
         }, {
-            name : "QX50",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2522"
-        }, {
-            name : "QX60",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2523"
-        }, {
-            name : "QX70",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2524"
-        }, {
-            name : "QX80",
-            url : "http://bose.cn/auto_listpage.aspx?nid=2525"
-        }]
+            "name" : "Escalade Hybrid",
+            "url" : "http://bose.cn/auto_listpage.aspx?nid=2541",
+            "id" : 2541
+        }],
+        "image" : "http://bose.cn/upfile/20140916110942_1.jpg"
     }];
-    _.each(CARS, function(car) {
-        car.id = +car.url.slice(-3);
-        _.each(car.series, function(serie) {
-            serie.id = +serie.url.slice(-4);
-        });
-    })
 })();
 
-/*
 (function FUCK_YOU_BOSE_I_AM_A_SPIDER() {
-    var brandList = [{name:"奥迪",url:"http://bose.cn/auto_list.aspx?flid=282"},{name:"别克",url:"http://bose.cn/auto_list.aspx?flid=283"},{name:"红旗",url:"http://bose.cn/auto_list.aspx?flid=284"},{name:"雪佛兰",url:"http://bose.cn/auto_list.aspx?flid=285"},{name:"马自达",url:"http://bose.cn/auto_list.aspx?flid=286"},{name:"日产",url:"http://bose.cn/auto_list.aspx?flid=287"},{name:"英菲尼迪",url:"http://bose.cn/auto_list.aspx?flid=288"},{name:"雷诺",url:"http://bose.cn/auto_list.aspx?flid=289"},{name:"绅宝",url:"http://bose.cn/auto_list.aspx?flid=290"},{name:"保时捷",url:"http://bose.cn/auto_list.aspx?flid=291"},{name:"凯迪拉克",url:"http://bose.cn/auto_list.aspx?flid=292"}];
+    var brandList = [{
+        name : "奥迪",
+        url : "http://bose.cn/auto_list.aspx?flid=282"
+    }, {
+        name : "别克",
+        url : "http://bose.cn/auto_list.aspx?flid=283"
+    }, {
+        name : "红旗",
+        url : "http://bose.cn/auto_list.aspx?flid=284"
+    }, {
+        name : "雪佛兰",
+        url : "http://bose.cn/auto_list.aspx?flid=285"
+    }, {
+        name : "马自达",
+        url : "http://bose.cn/auto_list.aspx?flid=286"
+    }, {
+        name : "日产",
+        url : "http://bose.cn/auto_list.aspx?flid=287"
+    }, {
+        name : "英菲尼迪",
+        url : "http://bose.cn/auto_list.aspx?flid=288"
+    }, {
+        name : "雷诺",
+        url : "http://bose.cn/auto_list.aspx?flid=289"
+    }, {
+        name : "绅宝",
+        url : "http://bose.cn/auto_list.aspx?flid=290"
+    }, {
+        name : "保时捷",
+        url : "http://bose.cn/auto_list.aspx?flid=291"
+    }, {
+        name : "凯迪拉克",
+        url : "http://bose.cn/auto_list.aspx?flid=292"
+    }];
     _.each(brandList, function(brand) {
+        brand.id = +brand.url.slice(-3);
         var url = brand.url;
         brand.series = [];
         $.get(url, function(data) {
-            var rows = $(data).find('.brands_row');
-            _.each(rows, function(row) {
-                brand.series.push({
-                    name : $(row).find('.brands_name').text(),
-                    url : 'http://bose.cn/' + $(row).find('.brands_more a').attr('href')
-                });
+            var $data = $(data);
+            brand.image = 'http://bose.cn/' + $data.find('.brands_banner img').attr('src');
+            $data.find('.brands_row').each(function() {
+                var serie = {
+                    name : $(this).find('.brands_name').text(),
+                    url : 'http://bose.cn/' + $(this).find('.brands_more a').attr('href')
+                };
+                serie.id = +serie.url.slice(-4);
+                brand.series.push(serie);
             });
         });
     });
+    window.brandList = brandList;
 })();
-*/
