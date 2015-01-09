be5e41d6fdfbb4ad542395a4ecce87854f745320
Xindong Ding <dxd.spirits@gmail.com>
2015-01-09 16:35:05 +0800

    add new page jobs-all

--- assets/javascripts/JOBS.js
+++ assets/javascripts/JOBS.js
@@ -32,8 +32,74 @@
 			]
 		}
 	];
+	window.JOBSALL = [
+		{
+			id: 1,
+			category: 'RDG(零售业务部)',
+			positions: [
+				{id: 1001, nameEN: 'District Manager', name: '区域零售业务经理', location: 'Shanghai'},
+				{id: 1002, nameEN: 'District Manager', name: '区域零售业务经理', location: 'Guangzhou'},
+				{id: 1003, nameEN: 'District Manager', name: '区域零售业务经理', location: 'Beijing'},
+				{id: 1004, nameEN: 'Store Manager', name: '专卖店店长', location: 'Shenyang'},
+				{id: 1005, nameEN: 'Store Manager', name: '专卖店店长', location: 'Wuxi'},
+				{id: 1006, nameEN: 'Store Manager', name: '专卖店店长', location: 'Shanghai'},
+				{id: 1007, nameEN: 'Store Sales', name: '专卖店店员', location: 'Shanghai'},
+				{id: 1008, nameEN: 'Store Sales', name: '专卖店店员', location: 'Hangzhou'},
+				{id: 1009, nameEN: 'Store Sales', name: '专卖店店员', location: 'Suzhou'},
+				{id: 1010, nameEN: 'Store Sales', name: '专卖店店员', location: 'Wuxi'},
+				{id: 1011, nameEN: 'Store Sales', name: '专卖店店员', location: 'Nanjing'},
+				{id: 1012, nameEN: 'Store Sales', name: '专卖店店员', location: 'Changzhou'},
+				{id: 1013, nameEN: 'Store Sales', name: '专卖店店员', location: 'Yangzhou'},
+				{id: 1014, nameEN: 'Store Sales', name: '专卖店店员', location: 'Xuzhou'},
+				{id: 1015, nameEN: 'Store Sales', name: '专卖店店员', location: 'Wuhan'},
+				{id: 1016, nameEN: 'Store Sales', name: '专卖店店员', location: 'Dongguan'},
+				{id: 1017, nameEN: 'Store Sales', name: '专卖店店员', location: 'Guangzhou'},
+				{id: 1018, nameEN: 'Store Sales', name: '专卖店店员', location: 'Panyu'},
+				{id: 1019, nameEN: 'Store Sales', name: '专卖店店员', location: 'Shenzhen'},
+				{id: 1020, nameEN: 'Store Sales', name: '专卖店店员', location: 'Kunming'},
+				{id: 1021, nameEN: 'Store Sales', name: '专卖店店员', location: 'Nanning'},
+				{id: 1022, nameEN: 'Store Sales', name: '专卖店店员', location: 'Chongqing'},
+				{id: 1023, nameEN: 'Store Sales', name: '专卖店店员', location: 'Lanzhou'},
+				{id: 1024, nameEN: 'Store Sales', name: '专卖店店员', location: 'Yinchuan'},
+				{id: 1025, nameEN: 'Store Sales', name: '专卖店店员', location: 'Shenyang'},
+				{id: 1026, nameEN: 'Store Sales', name: '专卖店店员', location: 'Tianjin'},
+				{id: 1027, nameEN: 'Store Sales', name: '专卖店店员', location: 'Changchun'},
+				{id: 1028, nameEN: 'Store Sales', name: '专卖店店员', location: 'Beijing'},
+				{id: 1029, nameEN: 'Store Sales', name: '专卖店店员', location: 'Harbin'},
+				{id: 1030, nameEN: 'Store Sales', name: '专卖店店员', location: 'Jinan'},
+				{id: 1031, nameEN: 'Store Sales', name: '专卖店店员', location: 'Hohhot'}
+			]
+		}, {
+			id: 2,
+			category: 'Function',
+			positions: [
+				{id: 2001, nameEN: 'Sales Training Manager', name: '培训经理', location: 'Shanghai'},
+				{id: 2002, nameEN: 'Logistic Assistant', name: '物流助理', location: 'Shanghai'},
+				{id: 2003, nameEN: 'Customer Service Representative', name: '客户服务代表', location: 'Shanghai'}
+			]
+		}, {
+			id: 3,
+			category: 'PSD(专业系统部)',
+			positions: [
+				{id: 3001, nameEN: 'Dealer Account Manager', name: '渠道客户经理（北方地区）', location: 'Shanghai'},
+				{id: 3002, nameEN: 'Dealer Account Manager', name: '渠道客户经理（江苏、安徽区域）', location: 'Shanghai'},
+				{id: 3003, nameEN: 'Dealer Account Manager', name: '渠道客户经理（浙江区域）', location: 'Shanghai'},
+				{id: 3004, nameEN: 'Dealer Account Manager', name: '渠道客户经理（湖北、湖南、江西区域）', location: 'Shanghai'},
+				{id: 3005, nameEN: 'Dealer Account Manager', name: '渠道客户经理（西北区域）', location: 'Shanghai'},
+				{id: 3006, nameEN: 'Dealer Account Manager', name: '渠道客户经理（中南区域）', location: 'Shanghai'},
+				{id: 3007, nameEN: 'Senior Design Engineer', name: '高级系统设计工程师', location: 'Shanghai'},
+				{id: 3008, nameEN: 'Operation Assistant', name: '业务助理', location: 'Shanghai'},
+				{id: 3009, nameEN: 'Trainer', name: '专业产品培训师', location: 'Shanghai'}
+			]
+		}
+	];
 	window.JOB_POSITIONS = {};
 	_.each(JOBS, function(category) {
+		_.each(category.positions, function(position) {
+			JOB_POSITIONS[position.id] = position;
+		});
+	});
+	_.each(JOBSALL, function(category) {
 		_.each(category.positions, function(position) {
 			JOB_POSITIONS[position.id] = position;
 		});
