807538fd7df3e2ad970754bf692be55bb463df68
Xindong Ding <dxd.spirits@gmail.com>
2015-01-09 16:08:42 +0800

    order car brands, modify video wrapper

--- assets/javascripts/CARS.js
+++ assets/javascripts/CARS.js
@@ -3,8 +3,8 @@
         name : '奥迪',
         id : 'audi',
         series : [{
-            name : 'Q3',
-            id : 'q3'
+            name : 'Audi A1',
+            id : 'a1'
         }, {
             name : 'A6L/S6',
             id : 'a6'
@@ -15,8 +15,8 @@
             name : 'A8L/S8',
             id : 'a8'
         }, {
-            name : 'Audi A1',
-            id : 'a1'
+            name : 'Q3',
+            id : 'q3'
         }, {
             name : 'Q7',
             id : 'q7'
@@ -28,11 +28,36 @@
             id : 'ttr'
         }]
     }, {
+        name : '保时捷',
+        id : 'porsche',
+        series : [{
+            name : '911 Carrera',
+            id : 'carrera'
+        }, {
+            name : 'Boxster',
+            id : 'boxster'
+        }, {
+            name : 'Cayenne卡宴',
+            id : 'cayenne'
+        }, {
+            name : 'Cayman',
+            id : 'cayman'
+        }, {
+            name : 'Macan',
+            id : 'macan'
+        }, {
+            name : 'Panamera',
+            id : 'panamera'
+        }]
+    }, {
         name : '别克',
         id : 'buick',
         series : [{
             name : 'Enclave昂科雷',
             id : 'enclave'
+        }, {
+            name : 'Envision昂科威',
+            id : 'envision'
         }, {
             name : 'GL8 豪华商务车',
             id : 'gl8'
@@ -42,9 +67,6 @@
         }, {
             name : 'Regal君威',
             id : 'regal'
-        }, {
-            name : 'Envision昂科威',
-            id : 'envision'
         }]
     }, {
         name : '红旗',
@@ -57,19 +79,44 @@
             id : 'l5'
         }]
     }, {
-        name : '雪佛兰',
-        id : 'chevrolet',
+        name : '凯迪拉克',
+        id : 'cadillac',
         series : [{
-            name : 'Volt',
-            id : 'volt'
+            name : 'ATS-L',
+            id : 'ats'
+        }, {
+            name : 'CTS',
+            id : 'cts'
+        }, {
+            name : 'Escalade Hybrid',
+            id : 'escalade'
+        }, {
+            name : 'SRX',
+            id : 'srx'
+        }, {
+            name : 'XTS',
+            id : 'xts'
+        }]
+    }, {
+        name : '雷诺',
+        id : 'renault',
+        series : [{
+            name : 'Koleos科雷傲',
+            id : 'koleos'
+        }, {
+            name : 'Latitude纬度',
+            id : 'latitude'
+        }, {
+            name : 'Talisman塔利斯曼',
+            id : 'talisman'
+        }, {
+            name : 'Fluence风朗',
+            id: 'fluence'
         }]
     }, {
         name : '马自达',
         id : 'mazda',
         series : [{
-            name : 'Mazda MX-5',
-            id : 'mx5'
-        }, {
             name : 'Mazda ATENZA阿特兹',
             id : 'atenza'
         }, {
@@ -78,31 +125,51 @@
         }, {
             name : 'Mazda CX-7',
             id : 'cx7'
+        }, {
+            name : 'Mazda MX-5',
+            id : 'mx5'
         }]
     }, {
         name : '日产',
         id : 'nissan',
         series : [{
-            name : 'Teana 天籁',
-            id : 'teana'
-        }, {
-            name : 'Teana 天籁▪公爵',
-            id : 'teanaduke'
-        }, {
-            name : 'GTR',
-            id : 'gtr'
-        }, {
             name : '370Z',
             id : '370z'
         }, {
-            name : 'QUEST 贵士',
-            id : 'quest'
+            name : 'GTR',
+            id : 'gtr'
         }, {
             name : 'MURANO 楼兰',
             id : 'murano'
         }, {
             name : 'Patrol途乐',
             id : 'patrol'
+        }, {
+            name : 'QUEST 贵士',
+            id : 'quest'
+        }, {
+            name : 'Teana 天籁',
+            id : 'teana'
+        }, {
+            name : 'Teana 天籁▪公爵',
+            id : 'teanaduke'
+        }]
+    }, {
+        name : '绅宝',
+        id : 'senova',
+        series : [{
+            name : 'D60',
+            id : 'd60'
+        }, {
+            name : 'D70',
+            id : 'd70'
+        }]
+    }, {
+        name : '雪佛兰',
+        id : 'chevrolet',
+        series : [{
+            name : 'Volt',
+            id : 'volt'
         }]
     }, {
         name : '英菲尼迪',
@@ -111,11 +178,11 @@
             name : 'Q50',
             id : 'q50'
         }, {
-            name : 'Q60S',
-            id : 'q60s'
-        }, {
             name : 'Q60',
             id : 'q60'
+        }, {
+            name : 'Q60S',
+            id : 'q60s'
         }, {
             name : 'Q70L',
             id : 'q70l'
@@ -131,73 +198,6 @@
         }, {
             name : 'QX80',
             id : 'qx80'
-        }]
-    }, {
-        name : '雷诺',
-        id : 'renault',
-        series : [{
-            name : 'Latitude纬度',
-            id : 'latitude'
-        }, {
-            name : 'Talisman塔利斯曼',
-            id : 'talisman'
-        }, {
-            name : 'Koleos科雷傲',
-            id : 'koleos'
-        }, {
-        	name : 'Fluence风朗',
-        	id: 'fluence'
-        }]
-    }, {
-        name : '绅宝',
-        id : 'senova',
-        series : [{
-            name : 'D70',
-            id : 'd70'
-        }, {
-            name : 'D60',
-            id : 'd60'
-        }]
-    }, {
-        name : '保时捷',
-        id : 'porsche',
-        series : [{
-            name : 'Cayman',
-            id : 'cayman'
-        }, {
-            name : 'Boxster',
-            id : 'boxster'
-        }, {
-            name : '911 Carrera',
-            id : 'carrera'
-        }, {
-            name : 'Panamera',
-            id : 'panamera'
-        }, {
-            name : 'Cayenne卡宴',
-            id : 'cayenne'
-        }, {
-            name : 'Macan',
-            id : 'macan'
-        }]
-    }, {
-        name : '凯迪拉克',
-        id : 'cadillac',
-        series : [{
-            name : 'ATS-L',
-            id : 'ats'
-        }, {
-            name : 'CTS',
-            id : 'cts'
-        }, {
-            name : 'XTS',
-            id : 'xts'
-        }, {
-            name : 'SRX',
-            id : 'srx'
-        }, {
-            name : 'Escalade Hybrid',
-            id : 'escalade'
         }]
     }];
 })();
