f000903af017ee4d9be9ed1616dc22ae9fffc8c7
Xindong Ding <dxd.spirits@gmail.com>
2015-02-05 17:47:19 +0800

    modify jobs list and job descriptions

--- assets/javascripts/JOBS.js
+++ assets/javascripts/JOBS.js
@@ -4,94 +4,83 @@
 			id: 1,
 			category: 'RDG(零售业务部)',
 			positions: [
-				{id: 100, nameEN: 'District Manager', name: '区域零售业务经理', location:'Shanghai'},
-				{id: 101, nameEN: 'Store Manager', name: '专卖店店长', location:'Shenyang'},
-				{id: 102, nameEN: 'Store Manager', name: '专卖店店长', location:'Wuxi'},
-				{id: 103, nameEN: 'Store Manager', name: '专卖店店长', location:'Shanghai'},
-				{id: 104, nameEN: 'Store Sales', name: '专卖店店员', location:'Hangzhou'},
-				{id: 105, nameEN: 'Store Sales', name: '专卖店店员', location:'Suzhou'},
-				{id: 106, nameEN: 'Store Sales', name: '专卖店店员', location:'Wuxi'},
-				{id: 107, nameEN: 'Store Sales', name: '专卖店店员', location:'Shenyang'},
-				{id: 108, nameEN: 'Store Sales', name: '专卖店店员', location:'Tianjin'},
-				{id: 109, nameEN: 'Store Sales', name: '专卖店店员', location:'Changchun'}
+				{id: 101, nameEN: 'Store Manager', name: '专卖店店长', locationEN:'Shenyang', location:'上海'},
+				{id: 102, nameEN: 'Store Manager', name: '专卖店店长', locationEN:'Wuxi', location:'无锡'},
+				{id: 104, nameEN: 'Store Sales', name: '专卖店店员', locationEN:'Hangzhou', location:'杭州'},
+				{id: 105, nameEN: 'Store Sales', name: '专卖店店员', locationEN:'Suzhou', location:'苏州'},
+				{id: 106, nameEN: 'Store Sales', name: '专卖店店员', locationEN:'Wuxi', location:'无锡'},
+				{id: 108, nameEN: 'Store Sales', name: '专卖店店员', locationEN:'Shanghai', location:'上海'},
+				{id: 107, nameEN: 'Store Sales', name: '专卖店店员', locationEN:'Shenyang', location:'沈阳'},
+				{id: 109, nameEN: 'Store Sales', name: '专卖店店员', locationEN:'Changchun', location:'长春'}
 			]
 		}, {
 			id: 2,
 			category: 'Function',
 			positions: [
-				{id: 200, nameEN: 'Sales Training Manager', name: '培训经理', location: 'Shanghai'},
-				{id: 201, nameEN: 'Logistic Assistant', name: '物流助理', location: 'Shanghai'}
+				{id: 200, nameEN: 'Sales Training Manager', name: '培训经理', locationEN: 'Shanghai', location:'上海'},
+				{id: 201, nameEN: 'Assistant Repair Engineer', name: '助理维修工程师', locationEN: 'Shanghai', location:'上海'},
+				{id: 202, nameEN: 'Online Business Supervisor', name: '电子商务主管', locationEN: 'Shanghai', location:'上海'}
 			]
-		}, {
+		}/*, {
 			id: 3,
 			category: 'PSD(专业系统部)',
 			positions: [
-				{id: 300, nameEN: 'Dealer Account Manager', name: '渠道客户经理（江苏、安徽区域）', location: 'Shanghai'},
-				{id: 301, nameEN: 'Dealer Account Manager', name: '渠道客户经理（浙江区域）', location: 'Shanghai'},
-				{id: 302, nameEN: 'Operation Assistant', name: '业务助理', location: 'Shanghai'}
+				{id: 300, nameEN: 'Dealer Account Manager', name: '渠道客户经理（江苏、安徽区域）', locationEN: 'Shanghai', location:'上海'},
+				{id: 301, nameEN: 'Dealer Account Manager', name: '渠道客户经理（浙江区域）', locationEN: 'Shanghai', location:'上海'},
+				{id: 302, nameEN: 'Operation Assistant', name: '业务助理', locationEN: 'Shanghai', location:'上海'}
 			]
-		}
+		}*/
 	];
 	window.JOBSALL = [
 		{
 			id: 1,
 			category: 'RDG(零售业务部)',
 			positions: [
-				{id: 1001, nameEN: 'District Manager', name: '区域零售业务经理', location: 'Shanghai'},
-				{id: 1002, nameEN: 'District Manager', name: '区域零售业务经理', location: 'Guangzhou'},
-				{id: 1003, nameEN: 'District Manager', name: '区域零售业务经理', location: 'Beijing'},
-				{id: 1004, nameEN: 'Store Manager', name: '专卖店店长', location: 'Shenyang'},
-				{id: 1005, nameEN: 'Store Manager', name: '专卖店店长', location: 'Wuxi'},
-				{id: 1006, nameEN: 'Store Manager', name: '专卖店店长', location: 'Shanghai'},
-				{id: 1007, nameEN: 'Store Sales', name: '专卖店店员', location: 'Shanghai'},
-				{id: 1008, nameEN: 'Store Sales', name: '专卖店店员', location: 'Hangzhou'},
-				{id: 1009, nameEN: 'Store Sales', name: '专卖店店员', location: 'Suzhou'},
-				{id: 1010, nameEN: 'Store Sales', name: '专卖店店员', location: 'Wuxi'},
-				{id: 1011, nameEN: 'Store Sales', name: '专卖店店员', location: 'Nanjing'},
-				{id: 1012, nameEN: 'Store Sales', name: '专卖店店员', location: 'Changzhou'},
-				{id: 1013, nameEN: 'Store Sales', name: '专卖店店员', location: 'Yangzhou'},
-				{id: 1014, nameEN: 'Store Sales', name: '专卖店店员', location: 'Xuzhou'},
-				{id: 1015, nameEN: 'Store Sales', name: '专卖店店员', location: 'Wuhan'},
-				{id: 1016, nameEN: 'Store Sales', name: '专卖店店员', location: 'Dongguan'},
-				{id: 1017, nameEN: 'Store Sales', name: '专卖店店员', location: 'Guangzhou'},
-				{id: 1018, nameEN: 'Store Sales', name: '专卖店店员', location: 'Panyu'},
-				{id: 1019, nameEN: 'Store Sales', name: '专卖店店员', location: 'Shenzhen'},
-				{id: 1020, nameEN: 'Store Sales', name: '专卖店店员', location: 'Kunming'},
-				{id: 1021, nameEN: 'Store Sales', name: '专卖店店员', location: 'Nanning'},
-				{id: 1022, nameEN: 'Store Sales', name: '专卖店店员', location: 'Chongqing'},
-				{id: 1023, nameEN: 'Store Sales', name: '专卖店店员', location: 'Lanzhou'},
-				{id: 1024, nameEN: 'Store Sales', name: '专卖店店员', location: 'Yinchuan'},
-				{id: 1025, nameEN: 'Store Sales', name: '专卖店店员', location: 'Shenyang'},
-				{id: 1026, nameEN: 'Store Sales', name: '专卖店店员', location: 'Tianjin'},
-				{id: 1027, nameEN: 'Store Sales', name: '专卖店店员', location: 'Changchun'},
-				{id: 1028, nameEN: 'Store Sales', name: '专卖店店员', location: 'Beijing'},
-				{id: 1029, nameEN: 'Store Sales', name: '专卖店店员', location: 'Harbin'},
-				{id: 1030, nameEN: 'Store Sales', name: '专卖店店员', location: 'Jinan'},
-				{id: 1031, nameEN: 'Store Sales', name: '专卖店店员', location: 'Hohhot'}
+				//{id: 1001, nameEN: 'District Manager', name: '区域零售业务经理', locationEN: 'Shanghai', location:'上海'},
+				//{id: 1002, nameEN: 'District Manager', name: '区域零售业务经理', locationEN: 'Guangzhou', location:'上海'},
+				//{id: 1003, nameEN: 'District Manager', name: '区域零售业务经理', locationEN: 'Beijing', location:'上海'},
+				{id: 1004, nameEN: 'Store Manager', name: '专卖店店长', locationEN: 'Shenyang', location:'沈阳'},
+				{id: 1005, nameEN: 'Store Manager', name: '专卖店店长', locationEN: 'Wuxi', location:'无锡'},
+				{id: 1007, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Shanghai', location:'上海'},
+				{id: 1008, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Hangzhou', location:'杭州'},
+				{id: 1009, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Suzhou', location:'苏州'},
+				{id: 1010, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Wuxi', location:'无锡'},
+				{id: 1012, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Changzhou', location:'常州'},
+				{id: 1013, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Yangzhou', location:'扬州'},
+				{id: 1014, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Xuzhou', location:'徐州'},
+				{id: 1016, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Dongguan', location:'东莞'},
+				{id: 1017, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Guangzhou', location:'广州'},
+				{id: 1020, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Kunming', location:'昆明'},
+				{id: 1025, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Shenyang', location:'沈阳'},
+				{id: 1027, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Changchun', location:'长春'},
+				{id: 1031, nameEN: 'Store Sales', name: '专卖店店员', locationEN: 'Qingdao', location:'青岛'}
 			]
 		}, {
 			id: 2,
 			category: 'Function',
 			positions: [
-				{id: 2001, nameEN: 'Sales Training Manager', name: '培训经理', location: 'Shanghai'},
-				{id: 2002, nameEN: 'Logistic Assistant', name: '物流助理', location: 'Shanghai'},
-				{id: 2003, nameEN: 'Customer Service Representative', name: '客户服务代表', location: 'Shanghai'}
+				{id: 2001, nameEN: 'Sales Training Manager', name: '培训经理', locationEN: 'Shanghai', location:'上海'},
+				{id: 2002, nameEN: 'Assistant Repair Engineer', name: '助理维修工程师', locationEN: 'Shanghai', location:'上海'},
+				{id: 2003, nameEN: 'Online Business Supervisor', name: '电子商务主管', locationEN: 'Shanghai', location:'上海'},
+				{id: 2004, nameEN: 'Sales Assistant', name: '销售助理', locationEN: 'Shanghai', location:'上海'},
+				{id: 2005, nameEN: 'Customer Service Representative', name: '客户服务代表', locationEN: 'Shanghai', location:'上海'},
+				{id: 2006, nameEN: 'Logistic Assistant', name: '物流助理', locationEN: 'Shanghai', location:'上海'}
 			]
-		}, {
+		}/*, {
 			id: 3,
 			category: 'PSD(专业系统部)',
 			positions: [
-				{id: 3001, nameEN: 'Dealer Account Manager', name: '渠道客户经理（北方地区）', location: 'Shanghai'},
-				{id: 3002, nameEN: 'Dealer Account Manager', name: '渠道客户经理（江苏、安徽区域）', location: 'Shanghai'},
-				{id: 3003, nameEN: 'Dealer Account Manager', name: '渠道客户经理（浙江区域）', location: 'Shanghai'},
-				{id: 3004, nameEN: 'Dealer Account Manager', name: '渠道客户经理（湖北、湖南、江西区域）', location: 'Shanghai'},
-				{id: 3005, nameEN: 'Dealer Account Manager', name: '渠道客户经理（西北区域）', location: 'Shanghai'},
-				{id: 3006, nameEN: 'Dealer Account Manager', name: '渠道客户经理（中南区域）', location: 'Shanghai'},
-				{id: 3007, nameEN: 'Senior Design Engineer', name: '高级系统设计工程师', location: 'Shanghai'},
-				{id: 3008, nameEN: 'Operation Assistant', name: '业务助理', location: 'Shanghai'},
-				{id: 3009, nameEN: 'Trainer', name: '专业产品培训师', location: 'Shanghai'}
+				{id: 3001, nameEN: 'Dealer Account Manager', name: '渠道客户经理（北方地区）', locationEN: 'Shanghai', location:'上海'},
+				{id: 3002, nameEN: 'Dealer Account Manager', name: '渠道客户经理（江苏、安徽区域）', locationEN: 'Shanghai', location:'上海'},
+				{id: 3003, nameEN: 'Dealer Account Manager', name: '渠道客户经理（浙江区域）', locationEN: 'Shanghai', location:'上海'},
+				{id: 3004, nameEN: 'Dealer Account Manager', name: '渠道客户经理（湖北、湖南、江西区域）', locationEN: 'Shanghai', location:'上海'},
+				{id: 3005, nameEN: 'Dealer Account Manager', name: '渠道客户经理（西北区域）', locationEN: 'Shanghai', location:'上海'},
+				{id: 3006, nameEN: 'Dealer Account Manager', name: '渠道客户经理（中南区域）', locationEN: 'Shanghai', location:'上海'},
+				{id: 3007, nameEN: 'Senior Design Engineer', name: '高级系统设计工程师', locationEN: 'Shanghai', location:'上海'},
+				{id: 3008, nameEN: 'Operation Assistant', name: '业务助理', locationEN: 'Shanghai', location:'上海'},
+				{id: 3009, nameEN: 'Trainer', name: '专业产品培训师', locationEN: 'Shanghai', location:'上海'}
 			]
-		}
+		}*/
 	];
 	window.JOB_POSITIONS = {};
 	_.each(JOBS, function(category) {
